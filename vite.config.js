905ac404fdcc092f91547f545168226ae82bfa24
Pierre Bertrand <pierre.bertrand.professionnel@gmail.com>
2025-05-14 12:41:03 +0200

    fix(config): update config for production build

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,51 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import fs from 'fs'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'localhost+2.pem')),
-    },
-    host: 'localhost',
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/assets/styles/abstracts/index" as *;',
+export default defineConfig(({ command, mode }) => {
+  const common = {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/abstracts/index" as *;',
+        },
+      },
+    },
+  }
+
+  if (command === 'serve') {
+    // mode = 'development' par défaut
+    return {
+      ...common,
+      server: {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'certs', 'localhost+2-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'localhost+2.pem')),
+        },
+        host: 'localhost',
+        port: 3000,
+      }
+    }
+  } else {
+    // build / preview
+    return {
+      ...common,
+      // pas de https local ici
+      server: {
+        host: '0.0.0.0',
+      }
+    }
+  }
 })
