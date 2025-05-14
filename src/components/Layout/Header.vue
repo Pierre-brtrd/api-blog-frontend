11cbf959c9d8c445914f87fe01567230d50e9ca4
Pierre Bertrand <pierre.bertrand.professionnel@gmail.com>
2025-05-14 11:31:28 +0200

    chore: add link to logo home page

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="navbar">
         <nav class="navbar-content container">
-            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="80" height="80" />
+            <RouterLink to="/" class="navbar-logo">
+                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="80" height="80" />
+            </RouterLink>
             <ul class="navbar-links">
                 <li>
                     <RouterLink to="/">Home</RouterLink>
